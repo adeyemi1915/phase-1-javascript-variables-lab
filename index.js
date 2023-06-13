cda202b858655508313d4bdfaba2f1d3fcea69ba
adeyemi1915 <135009534+adeyemi1915@users.noreply.github.com>
2023-06-13 13:29:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+//  let age =12;
+
+// if (age >= 18) { 
+//     console.log("You are allowed to vote") ;
+// }else{
+//     console.log ("You need to be above 18 to vote."
+// }
+
+// let message = (age >18)
+
+
+const companyName = 'Scuber';
+   let mostProfitableNeighbourhood = 'chelsea'
+   let companyCeo = 'Susan Smith'
